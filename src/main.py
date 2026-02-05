bd45d2b101fd8960432010f72f1256177524b4e1
pim <johnextremesourcreme@gmail.com>
2026-02-05 16:36:38 +0100

    fix2?

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from shutil import copy, rmtree
 import sys
 def copy_from_static_to_public(folder_path,file_list):
     if folder_path == "":
-        print("removing public and making it")
+        # print("removing public and making it")
         rmtree("public")
         mkdir("public")
     for f in file_list:
@@ -38,7 +38,7 @@ def extract_title(markdown):
                 return line.lstrip()[2:].strip()
     raise ValueError
 def generate_page(from_path, template_path, dest_path, base_path):
-    print(f"generating page from {from_path} to {dest_path} using {template_path}")
+    # print(f"generating page from {from_path} to {dest_path} using {template_path}")
     from_content = Path(from_path).read_text()
     template_content = Path(template_path).read_text()
     html_node = markdown_to_html_node(from_content)
@@ -46,8 +46,9 @@ def generate_page(from_path, template_path, dest_path, base_path):
     title = extract_title(from_content)
     template_content = template_content.replace("{{ Title }}", title)
     template_content = template_content.replace("{{ Content }}", html_node_content)
-    template_content = template_content.replace('href="/', f'href="{base_path}/')
-    template_content = template_content.replace('src="/', f'src="{base_path}/')
+    template_content = template_content.replace('href="/', f'href="{base_path}')
+    template_content = template_content.replace('src="/', f'src="{base_path}')
+    print(dest_path)
     p = Path(dest_path); p.parent.mkdir(parents=True, exist_ok=True)
     p.write_text(template_content)
 def generate_page_recursively(dir_path_content, template_path, dest_dir_path, base_path):
@@ -55,6 +56,7 @@ def generate_page_recursively(dir_path_content, template_path, dest_dir_path, ba
     for content in content_list:
         content_path = f"{dir_path_content}/{content}"
         dir_path = f"{dest_dir_path}/{content}"
+        # print(dir_path)
         if path.isdir(content_path):
             generate_page_recursively(content_path,template_path,dir_path, base_path)
         else:
@@ -79,6 +81,7 @@ def main():
     # copy_from_static_to_public()
     from_path = project_root / "content"
     template_path = project_root / "template.html"
+    print(template_path)
 
     generate_page_recursively(from_path, template_path, dest_path, basepath)
 
